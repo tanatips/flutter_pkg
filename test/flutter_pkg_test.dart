7c05877269f8791c6cc5cde01ef3ad9f30e7f3e0
tanatips <tanatip_s@hotmail.com>
2022-08-10 14:50:16 +0700

    my package

---
+++ test/flutter_pkg_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_pkg/flutter_pkg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
